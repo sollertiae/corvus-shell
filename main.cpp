bd1498b6cfc5c3078173379de1301960a074d112
David <ldcc@proton.me>
2026-04-05 16:10:08 -0600

    adding builtin

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 #include <sys/wait.h> //required for wait
 #include <string> 
 #include <vector>
+#include <map>
+#include <functional>
+
+int builtin_cd(std::vector<char*>& args) {
+    if (args.size() < 2) {
+        std::cerr << "cd: missing argument" << "\n";
+        return -1;        
+    }
+    return chdir(args[1]);
+}
+int builtin_exit(std::vector<char*>& args);
+
+std::map<std::string, std::function<int(std::vector<char*>&) >> builtins_map = {
+    {"cd", builtin_cd}
+};
 
 std::vector<char*> read_input(char *input) {
     std::vector<char*> args;
@@ -24,18 +39,23 @@ int main() {
         std::cout << "corvus >> ";
         std::getline(std::cin, input);
         command = read_input(input.data());
-        pid_child = fork();
-
-        if (pid_child < 0) {
-            std::cerr << "[!] Error creating fork" << "\n";
-        } else if (pid_child == 0) {
-            std::cout << "[+] Executing the command on child fork" << "\n";
-            if (execvp(command[0], command.data()) < 0) {
-                std::cerr << "[!] Error executing the command" << command[0] << "\n";
-            }
-        } else {
-            wait_result = waitpid(pid_child, &stat_loc, WUNTRACED);
+        
+        if (builtins_map.count(command[0])) {
+            builtins_map[command[0]](command);
         }
-    }
+        else {
+            pid_child = fork();
+            if (pid_child < 0) {
+                std::cerr << "[!] Error creating fork" << "\n";
+            } else if (pid_child == 0) {
+                std::cout << "[+] Executing the command on child fork" << "\n";
+                if (execvp(command[0], command.data()) < 0) {
+                    std::cerr << "[!] Error executing the command" << command[0] << "\n";
+                }
+            } else {
+                wait_result = waitpid(pid_child, &stat_loc, WUNTRACED);
+            }
+        }
+    }    
     return 0;
 }
