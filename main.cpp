fa878a10c08dd39aba698eac710b61ba0d0a8787
David <ldcc@proton.me>
2026-04-05 20:13:38 -0600

    basic pipe implementation

--- main.cpp
+++ main.cpp
@@ -5,19 +5,48 @@
 #include <vector>
 #include <map>
 #include <functional>
+#include <cstdlib>
 
 int builtin_cd(std::vector<char*>& args) {
-    if (args.size() < 2) {
-        std::cerr << "cd: missing argument" << "\n";
+    if (args.size() < 2 || args[1] == nullptr) {
+        std::cerr << "[!] cd: missing argument" << "\n";
         return -1;        
     }
-    return chdir(args[1]);
+    if (chdir(args[1]) < 0) {
+        std::cerr << "[!] cd: " << args[1] << " not" << "\n";
+        return -1;
+    }
+    return 0;
 }
-int builtin_exit(std::vector<char*>& args);
+int builtin_exit(std::vector<char*>& args) {
+    exit(0);
+}
 
 std::map<std::string, std::function<int(std::vector<char*>&) >> builtins_map = {
-    {"cd", builtin_cd}
+    {"cd", builtin_cd},
+    {"exit", builtin_exit}
 };
+
+std::vector<std::string> split_pipes(const std::string& input) {
+    std::vector<std::string> commands;
+    std::string current;
+
+    for (char c : input) {
+        if (c == '|') {
+            if (!current.empty()) {
+                commands.push_back(current);
+                current.clear();
+            }
+        } else {
+            current += c;
+        }
+    }
+
+    if (!current.empty()) {
+        commands.push_back(current);
+    }
+    return commands;
+}
 
 std::vector<char*> read_input(char *input) {
     std::vector<char*> args;
@@ -38,22 +67,63 @@ int main() {
     while (true) {
         std::cout << "corvus >> ";
         std::getline(std::cin, input);
-        command = read_input(input.data());
-        
-        if (builtins_map.count(command[0])) {
-            builtins_map[command[0]](command);
-        }
-        else {
-            pid_child = fork();
-            if (pid_child < 0) {
-                std::cerr << "[!] Error creating fork" << "\n";
-            } else if (pid_child == 0) {
-                std::cout << "[+] Executing the command on child fork" << "\n";
+
+        if (input.empty())
+            continue;
+
+        std::vector<std::string> pipe_parse = split_pipes(input);
+        command = read_input(pipe_parse[0].data());
+        if (pipe_parse.size() > 1) {
+            int fd[2];
+            pid_t pid_child_p;
+            std::vector<char*> command_2 = read_input(pipe_parse[1].data());
+            pipe(fd);
+            pid_child_p = fork();
+            if (pid_child_p < 0) {
+                std::cerr << "[!] Error creating fork with pipes" << "\n";
+            } else if (pid_child_p == 0) {
+                std::cout << "Hello from child 1\n";
+                close(fd[0]);
+                dup2(fd[1], STDOUT_FILENO);
                 if (execvp(command[0], command.data()) < 0) {
-                    std::cerr << "[!] Error executing the command" << command[0] << "\n";
+                    std::cerr << "[!] Error executing the command " << command[0] << "\n";
                 }
             } else {
-                wait_result = waitpid(pid_child, &stat_loc, WUNTRACED);
+                std::cout << "Hello from child 2\n";
+                pid_t pid_child_p2 = fork();
+                if (pid_child_p2 < 0) {
+                    std::cerr << "[!] Error creating fork with pipes" << "\n";
+                }  else if (pid_child_p2 == 0) {
+                    close(fd[1]);
+                    dup2(fd[0], STDIN_FILENO);
+                    if (execvp(command_2[0], command_2.data()) < 0) {
+                        std::cerr << "[!] Error executing the command " << command[0] << "\n";
+                    }
+                } else {
+                    close(fd[0]);
+                    close(fd[1]);
+                    waitpid(pid_child_p, nullptr, 0);
+                    waitpid(pid_child_p2, nullptr, 0);
+                }
+            }
+
+        } else {
+            
+            
+            if (builtins_map.count(command[0])) {
+                builtins_map[command[0]](command);
+            }
+            else {
+                pid_child = fork();
+                if (pid_child < 0) {
+                    std::cerr << "[!] Error creating fork" << "\n";
+                } else if (pid_child == 0) {
+                    if (execvp(command[0], command.data()) < 0) {
+                        std::cerr << "[!] Error executing the command " << command[0] << "\n";
+                    }
+                } else {
+                    wait_result = waitpid(pid_child, &stat_loc, WUNTRACED);
+                }
             }
         }
     }    
